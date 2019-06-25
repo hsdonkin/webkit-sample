810af94239b8e3e6a3e91dee45455432d32e64c4
Student <example@example.com>
2019-06-25 09:34:59 -0700

    adds sample js importing

---
+++ src/buttonscript.js
@@ -0,0 +1,5 @@
+export function doSomeMath(a, b){
+  return a + b;
+}
+
+export var didSomeMath = doSomeMath(7,10);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-    import './styles.css';
-    import $ from 'jquery';
-    
+  import './styles.css';
+  import $ from 'jquery';
+
+  import {doSomeMath} from "./buttonscript.js";
+  import {didSomeMath} from "./buttonscript.js";
+
+  $(document).ready(function(){
+    $(".button").click(function(){
+      var x = doSomeMath(3,4);
+      console.log(x);
+      console.log(didSomeMath);
+    })
+
+  });
