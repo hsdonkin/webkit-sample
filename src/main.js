e6ef79351fc816ab6d92b0a4da9fdede970b7fc3
Student <example@example.com>
2019-06-25 09:15:32 -0700

    sample webkit build

---
+++ src/main.js
@@ -0,0 +1,3 @@
+    import './styles.css';
+    import $ from 'jquery';
+    
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+   new CleanWebpackPlugin(['dist']),
+   new HtmlWebpackPlugin({
+     title: 'Our Page',
+     template: './src/index.html',
+     inject: 'body'
+   })
+ ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
